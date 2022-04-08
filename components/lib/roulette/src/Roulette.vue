bbc6ff39920a5053d868a5e903910d5333305110
Eric <487737744@qq.com>
2022-04-08 22:10:36 +0800

    修复Roulette组件响应式丢失问题

--- components/lib/roulette/src/Roulette.vue
+++ components/lib/roulette/src/Roulette.vue
@@ -1,85 +1,134 @@
 <template>
-  <div class="pixel-roulette" @mouseenter="timerStop()" @mouseleave="timerStart()" :style="{width:width+'px',height:height+'px'}">
-      <div class="pixel-roulette-plate">
-          <div v-if="isArr" class="pixel-roulette-plate-arr" :style="{width:width+'px',height:height+'px'}" >
-              <transition-group enter-active-class="animate__animated animate__slideInRight"
-              leave-active-class="animate__animated animate__slideOutLeft" >
-                  <img v-show="data[curIndex-1] == d" v-for="d in data" :key="d" :src="d.imgUrl" alt="">
-              </transition-group>
-          </div>
+  <div
+    class="pixel-roulette"
+    @mouseenter="timerStop()"
+    @mouseleave="timerStart()"
+    :style="{width:options.width+'px',height:options.height+'px'}"
+  >
+    <div class="pixel-roulette-plate">
+      <div
+        v-if="isArr"
+        class="pixel-roulette-plate-arr"
+        :style="{width:options.width+'px',height:options.height+'px'}"
+      >
+        <transition-group
+          enter-active-class="animate__animated animate__slideInRight"
+          leave-active-class="animate__animated animate__slideOutLeft"
+        >
+          <img
+            v-show="options.data[curIndex-1] == d"
+            v-for="d in options.data"
+            :key="d"
+            :src="d.imgUrl"
+            alt=""
+          >
+        </transition-group>
       </div>
-      <div v-if="showTitle" class="pixel-roulette-titles">
-          <transition-group enter-active-class="animate__animated animate__fadeIn"
-              leave-active-class="animate__animated animate__fadeOut">
-              <div class="pixel-roulette-titles-block" v-show="data[curIndex-1] == d" v-for="d in data" :key="d" >
-                  <p :style="{...titleStyle}" >{{d.title}}</p>
-                  <p class="pixel-roulette-titles-subtitle" :style="{...subtitleStyle}" >{{d.subtitle}}</p>
-              </div>
-          </transition-group>
-      </div>
-      <transition 
-         enter-active-class="animate__animated animate__fadeIn"
-         leave-active-class="animate__animated animate__fadeOut">
-        <div  v-show="direction"  class="pixel-roulette-direction">
-              <slot :data="data[curIndex-1]"  name="direction" >
-
-              </slot>
+    </div>
+    <div
+      v-if="options.showTitle"
+      class="pixel-roulette-titles"
+    >
+      <transition-group
+        enter-active-class="animate__animated animate__fadeIn"
+        leave-active-class="animate__animated animate__fadeOut"
+      >
+        <div
+          class="pixel-roulette-titles-block"
+          v-show="options.data[curIndex-1] == d"
+          v-for="d in options.data"
+          :key="d"
+        >
+          <p :style="{...options.titleStyle}">{{d.title}}</p>
+          <p
+            class="pixel-roulette-titles-subtitle"
+            :style="{...options.subtitleStyle}"
+          >{{d.subtitle}}</p>
         </div>
-      </transition>
+      </transition-group>
+    </div>
+    <transition
+      enter-active-class="animate__animated animate__fadeIn"
+      leave-active-class="animate__animated animate__fadeOut"
+    >
+      <div
+        v-show="direction"
+        class="pixel-roulette-direction"
+      >
+        <slot
+          :data="options.data[curIndex-1]"
+          name="direction"
+        >
+
+        </slot>
+      </div>
+    </transition>
   </div>
 </template>
 
 <script>
-import { defineComponent,reactive,toRefs,onBeforeMount,onUnmounted } from "vue"
-import renderCheack from '../../../hooks/renderCheack'
-import Scroll from '../../../hooks/Scroll'
+import {
+  defineComponent,
+  reactive,
+  toRefs,
+  onBeforeMount,
+  onUnmounted,
+  watchEffect,
+} from "vue";
+import renderCheack from "../../../hooks/renderCheack";
+import Scroll from "../../../hooks/Scroll";
 export default defineComponent({
-    name:'pix-roulette',
-    props: {
-        options:{type: Object,default:()=>{}}
-    },
-    setup(props){
-        let options = reactive({
-            width:300,
-            height:200,
-            render:'arr',
-            data:[],
-            speed:1000,
-            titleStyle:{},
-            subtitleStyle:{},
-            showTitle:true,
-            ...props.options
-        })
-        let model = ['arr'];
-        let config = reactive({
-            isArr:renderCheack(options.render,model,options.data),
-            curIndex:1,
-            direction:false
-        })
-        let timerID;
+  name: "pix-roulette",
+  props: {
+    options: { type: Object, default: () => {} },
+  },
+  setup(props) {
+    const state = reactive({
+      options: {
+        width: 300,
+        height: 200,
+        render: "arr",
+        data: [],
+        speed: 1000,
+        titleStyle: {},
+        subtitleStyle: {},
+        showTitle: true,
+        ...props.options,
+      },
+    });
+    let model = ["arr"];
+    let config = reactive({
+      isArr: renderCheack(state.options.render, model, state.options.data),
+      curIndex: 1,
+      direction: false,
+    });
+    let timerID;
 
-        function timerStart(){
-            timerID = setInterval(()=>{
-                config.curIndex = Scroll(config.curIndex,options.data.length);
-            },options.speed);
-            config.direction = false;
-        }
+    watchEffect(() => {
+      state.options = props.options;
+    });
 
-        function timerStop(){
-            clearInterval(timerID);
-            config.direction = true;
-        }
-
-        onBeforeMount(()=>{
-            timerStart();
-        })
-        
-        
-        onUnmounted(()=>{
-            timerStop()
-        })
-
-        return {...toRefs(config),...toRefs(options),timerStop,timerStart}
+    function timerStart() {
+      timerID = setInterval(() => {
+        config.curIndex = Scroll(config.curIndex, state.options.data.length);
+      }, state.options.speed);
+      config.direction = false;
     }
-})
+
+    function timerStop() {
+      clearInterval(timerID);
+      config.direction = true;
+    }
+
+    onBeforeMount(() => {
+      timerStart();
+    });
+
+    onUnmounted(() => {
+      timerStop();
+    });
+
+    return { ...toRefs(config), ...toRefs(state), timerStop, timerStart };
+  },
+});
 </script>
--- examples/demo/RouletteDemo.vue
+++ examples/demo/RouletteDemo.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="demo-roulette">
+    <pix-btn
+      @click="loadData"
+      :options="{
+          value:'加载新数据'
+      }"
+    />
     <pix-roulette :options="{
           data:d,
           width:800,
@@ -28,33 +34,53 @@
 </template>
 
 <script>
-import { defineComponent } from "vue";
+import { defineComponent, reactive, toRefs } from "vue";
 export default defineComponent({
   setup() {
-    let d = [
-      {
-        imgUrl: require("../assets/head-5928mm-Moral_Choices.jpg"),
-        title: "测试标题1",
-        subtitle: "这个标题1的副标题",
-        hot: "爆火人气",
-        badge: "扮演,解密",
-      },
-      {
-        imgUrl: require("../assets/head-6371eu-The_Forgotten_City.jpg"),
-        title: "测试标题2",
-        subtitle: "这个标题2的副标题",
-        hot: "爆火人气",
-        badge: "奇幻,动作",
-      },
-      {
-        imgUrl: require("../assets/l-3012688-YtKdaf.jpg"),
-        title: "测试标题3",
-        subtitle: "这个标题2的副标题",
-        hot: "爆火人气",
-        badge: "魔法,RPG",
-      },
-    ];
-    return { d };
+    let state = reactive({
+      d: [
+        {
+          imgUrl: require("../assets/head-5928mm-Moral_Choices.jpg"),
+          title: "测试标题1",
+          subtitle: "这个标题1的副标题",
+          hot: "爆火人气",
+          badge: "扮演,解密",
+        },
+        {
+          imgUrl: require("../assets/head-6371eu-The_Forgotten_City.jpg"),
+          title: "测试标题2",
+          subtitle: "这个标题2的副标题",
+          hot: "爆火人气",
+          badge: "奇幻,动作",
+        },
+        {
+          imgUrl: require("../assets/l-3012688-YtKdaf.jpg"),
+          title: "测试标题3",
+          subtitle: "这个标题2的副标题",
+          hot: "爆火人气",
+          badge: "魔法,RPG",
+        },
+      ],
+    });
+    function loadData() {
+      state.d = [
+        {
+          imgUrl: require("../assets/head-5928mm-Moral_Choices.jpg"),
+          title: "123123",
+          subtitle: "这个标题1的副标题",
+          hot: "爆火人气123123",
+          badge: "扮演,解密",
+        },
+        {
+          imgUrl: require("../assets/head-5928mm-Moral_Choices.jpg"),
+          title: "123123",
+          subtitle: "这个标题1的副标题",
+          hot: "爆火人气123123",
+          badge: "扮演,解密",
+        },
+      ];
+    }
+    return { ...toRefs(state), loadData };
   },
 });
 </script>
